fd09f923c68007beace371f1aaadaba9519eb4e0
Kh82Olga <91885710+Kh82Olga@users.noreply.github.com>
2022-06-07 15:47:06 +0200

    0706

--- VideoRental/Controllers/ActorDvdController.cs
+++ VideoRental/Controllers/ActorDvdController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VideoRental.Repositories;
 
 namespace VideoRental.Controllers
 {
     public class ActorDvdController : Controller
     {
+        private readonly ActorDvdRepo _actorDvdRepo;
+
+        public ActorDvdController(ActorDvdRepo actorDvdRepo)
+        {
+            _actorDvdRepo = actorDvdRepo;
+        }
         public IActionResult Index()
         {
             return View();
