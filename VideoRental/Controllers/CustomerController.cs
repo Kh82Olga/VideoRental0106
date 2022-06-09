5e45fc299db8fd67355bb8e654c2b2722ba51edc
Kh82Olga <kh82olga@protonmail.com>
2022-06-09 16:59:52 +0200

    home

--- VideoRental/Controllers/CustomerController.cs
+++ VideoRental/Controllers/CustomerController.cs
@@ -1,11 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VideoRental.Models;
+using VideoRental.Repositories;
 
 namespace VideoRental.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly CustomerRepo _customerRepo;
+
+        public CustomerController(CustomerRepo customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
         public IActionResult Index()
         {
+            var customers = _customerRepo.GetAll();
+            return View(customers);
+        }
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Insert(Customer customer)
+        {
+            _customerRepo.Insert(customer);
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var customer = _customerRepo.GetById(id);
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Update(Customer customer)
+        {
+            _customerRepo.Update(customer);
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var customer = _customerRepo.GetById(id);
+            return View(customer);
+        }
+        [HttpPost]
+        public IActionResult Delete(Customer customer)
+        {
+            _customerRepo.Delete(customer);
             return View();
         }
     }
--- VideoRental/Controllers/GenreController.cs
+++ VideoRental/Controllers/GenreController.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VideoRental.Models;
+using VideoRental.Repositories;
 
 namespace VideoRental.Controllers
 {
     public class GenreController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
+        
+            private readonly GenreRepo _genreRepo;
+
+            public GenreController(GenreRepo genreRepo)
+            {
+                _genreRepo = genreRepo;
+            }
+            public IActionResult Index()
+            {
+                var genres = _genreRepo.GetAll();
+                return View(genres);
+            }
+            [HttpGet]
+            public IActionResult Insert()
+            {
+                return View();
+            }
+            [HttpPost]
+            public IActionResult Insert(Genre genre)
+            {
+                _genreRepo.Insert(genre);
+                return View();
+            }
+            [HttpGet]
+            public IActionResult Update(int id)
+            {
+                var genre = _genreRepo.GetById(id);
+                return View(genre);
+            }
+            [HttpPost]
+            public IActionResult Update(Genre genre)
+            {
+                _genreRepo.Update(genre);
+                return View();
+            }
+            [HttpGet]
+            public IActionResult Delete(int id)
+            {
+                var genre = _genreRepo.GetById(id);
+                return View(genre);
+            }
+            [HttpPost]
+            public IActionResult Delete(Genre genre)
+            {
+                _genreRepo.Delete(genre);
+                return View();
+            }
+        
+}   }
